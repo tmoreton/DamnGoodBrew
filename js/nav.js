491ccb1d40bcff5359609e4be968f2edfb4a8a33
Tim Moreton <tmoreton89@gmail.com>
2018-06-01 12:13:54 -0400

    day 24 pop up windw

--- js/nav.js
+++ js/nav.js
@@ -22,3 +22,21 @@ $('#close-nav').click(function(){
 	$('#close-nav').addClass("hidden");
 });
 
+
+// Start Modal Function
+$('.open-modal').click(function(){
+	$('#modal').removeClass("hidden");
+});
+
+$('#close-modal').click(function(){
+	$('#modal').addClass("hidden");
+});
+// End Modal Function
+
+
+$( document ).ready(function() {
+	console.log("ready!")
+	setTimeout(function(){
+		$('#modal').removeClass("hidden");
+	}, 3000);
+});
