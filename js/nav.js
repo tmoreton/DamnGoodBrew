5e759353106364245dfe86919218bfb3cf03eff0
Tim Moreton <tmoreton89@gmail.com>
2018-05-31 21:37:57 -0400

    day 23 jquery vs javascript

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,24 @@
-function openNav(){
-	document.getElementById("nav").classList.remove("hidden-mobile")
-	document.getElementById("open-nav").classList.add("hidden")
-	document.getElementById("close-nav").classList.remove("hidden")
-}
+// function closeNav(){
+// 	document.getElementById("nav").classList.add("hidden-mobile")
+// 	document.getElementById("open-nav").classList.remove("hidden")
+// 	document.getElementById("close-nav").classList.add("hidden")
+// }
 
-function closeNav(){
-	document.getElementById("nav").classList.add("hidden-mobile")
-	document.getElementById("open-nav").classList.remove("hidden")
-	document.getElementById("close-nav").classList.add("hidden")
-}
+// function openNav(){
+// 	document.getElementById("nav").classList.remove("hidden-mobile")
+// 	document.getElementById("open-nav").classList.add("hidden")
+// 	document.getElementById("close-nav").classList.remove("hidden")
+// }
+
+$('#open-nav').click(function(){
+	$('#nav').removeClass("hidden-mobile");
+	$('#open-nav').addClass("hidden");
+	$('#close-nav').removeClass("hidden");
+});
+
+$('#close-nav').click(function(){
+	$('#nav').addClass("hidden-mobile");
+	$('#open-nav').removeClass("hidden");
+	$('#close-nav').addClass("hidden");
+});
+
